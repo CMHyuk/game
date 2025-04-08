0186f81b478963ec77e1e692952d2c6a19cf1348
CMHyuk <alsgur9710@naver.com>
2025-04-08 20:56:11 +0900

    사용자 IP 로그에 남기도록 설정

--- src/main/java/com/game/common/log/CorrelationIdFilter.java
+++ src/main/java/com/game/common/log/CorrelationIdFilter.java
@@ -1,10 +1,12 @@
 package com.game.common.log;
 
 import jakarta.servlet.*;
+import jakarta.servlet.http.HttpServletRequest;
 import org.slf4j.MDC;
 import org.springframework.core.Ordered;
 import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
+import org.springframework.util.StringUtils;
 
 import java.io.IOException;
 import java.util.UUID;
@@ -13,11 +15,29 @@ import java.util.UUID;
 @Order(Ordered.HIGHEST_PRECEDENCE)
 public class CorrelationIdFilter implements Filter {
 
+    private static final String CORRELATION_ID = "CORRELATION_ID";
+    private static final String IP = "IP";
+
     @Override
-    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws ServletException, IOException {
+    public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+        HttpServletRequest httpRequest = (HttpServletRequest) request;
+        String clientIp = extractClientIp(httpRequest);
         String correlationId = UUID.randomUUID().toString();
-        MDC.put("CORRELATION_ID", correlationId);
+
+        MDC.put(IP, clientIp);
+        MDC.put(CORRELATION_ID, correlationId);
+
         chain.doFilter(request, response);
+
         MDC.clear();
     }
+
+    private String extractClientIp(HttpServletRequest request) {
+        String header = request.getHeader("X-Forwarded-For");
+        if (StringUtils.hasText(header)) {
+            return header.split(",")[0].trim();
+        }
+        return request.getRemoteAddr();
+    }
+
 }
