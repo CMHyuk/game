3e60ddff3bb5e2f84b803780ba80c18b6d44c106
CMHyuk <alsgur9710@naver.com>
2025-04-11 20:29:14 +0900

    숫자 통계 도메인 테스트

--- src/test/java/com/game/command/numbergame/domain/NumberStatisticsTest.java
+++ src/test/java/com/game/command/numbergame/domain/NumberStatisticsTest.java
@@ -1,4 +1,23 @@
-import static org.junit.jupiter.api.Assertions.*;
+package com.game.command.numbergame.domain;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 class NumberStatisticsTest {
-  
+
+    @Test
+    void 숫자를_증가시킨다() {
+        // given
+        int number = 20;
+        int frequency = 1;
+
+        NumberStatistics numberStatistics = new NumberStatistics(number, frequency);
+
+        // when
+        numberStatistics.increaseFrequency();
+
+        // then
+        Assertions.assertThat(numberStatistics.getFrequency()).isEqualTo(2);
+    }
+
 }
