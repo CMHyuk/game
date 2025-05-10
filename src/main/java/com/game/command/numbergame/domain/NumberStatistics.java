8007b336791e51f6d7327e7507b584352d2ffcda
CMHyuk <alsgur9710@naver.com>
2025-05-10 15:27:20 +0900

    난이도별 숫자 검증 로직 추가

--- src/main/java/com/game/command/numbergame/domain/NumberStatistics.java
+++ src/main/java/com/game/command/numbergame/domain/NumberStatistics.java
@@ -1,15 +1,23 @@
 package com.game.command.numbergame.domain;
 
 import com.game.common.entity.BaseEntity;
+import com.game.common.exception.BusinessException;
 import jakarta.persistence.*;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 
+import static com.game.command.numbergame.exception.NumberGameException.INVALID_NUMBER_RANGE;
+
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 public class NumberStatistics extends BaseEntity {
+
+    public static final int MIN = 1;
+    private static final int EASY_MAX_NUM = 50;
+    private static final int NORMAL_MAX_NUM = 100;
+    private static final int HARD_MAX_NUM = 1000;
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
@@ -21,6 +29,7 @@ public class NumberStatistics extends BaseEntity {
     private int frequency;
 
     public NumberStatistics(Level level, int number, int frequency) {
+        validateNumberRange(level, number);
         this.level = level;
         this.number = number;
         this.frequency = frequency;
@@ -28,6 +37,18 @@ public class NumberStatistics extends BaseEntity {
 
     public void increaseFrequency() {
         this.frequency++;
+    }
+
+    private void validateNumberRange(Level level, int number) {
+        int max = switch (level) {
+            case EASY -> EASY_MAX_NUM;
+            case NORMAL -> NORMAL_MAX_NUM;
+            case HARD -> HARD_MAX_NUM;
+        };
+
+        if (number < MIN || number > max) {
+            throw BusinessException.from(INVALID_NUMBER_RANGE);
+        }
     }
 
 }
--- src/test/java/com/game/command/numbergame/domain/NumberStatisticsTest.java
+++ src/test/java/com/game/command/numbergame/domain/NumberStatisticsTest.java
@@ -1,7 +1,13 @@
 package com.game.command.numbergame.domain;
 
+import com.game.common.exception.BusinessException;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
+import static com.game.command.numbergame.exception.NumberGameException.INVALID_NUMBER_RANGE;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
 
 class NumberStatisticsTest {
 
@@ -19,6 +25,36 @@ class NumberStatisticsTest {
 
         // then
         Assertions.assertThat(numberStatistics.getFrequency()).isEqualTo(2);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "easy, 1, 10",
+            "normal, 2, 20",
+            "hard, 3, 30"
+    })
+    void 난이도와_숫자가_유효하면_정상적으로_생성된다(String level, int number, int frequency) {
+        // when & then
+        NumberStatistics result = new NumberStatistics(Level.from(level), number, frequency);
+        Assertions.assertThat(result.getLevel()).isEqualTo(Level.from(level));
+        Assertions.assertThat(result.getNumber()).isEqualTo(number);
+        Assertions.assertThat(result.getFrequency()).isEqualTo(frequency);
+    }
+
+    @ParameterizedTest
+    @CsvSource({
+            "easy, 0",
+            "easy, 51",
+            "normal, 0",
+            "normal, 101",
+            "hard, 0",
+            "hard, 1001"
+    })
+    void 난이도별_숫자범위_초과시_예외가_발생한다(String level, int number) {
+        // when & then
+        assertThatThrownBy(() -> new NumberStatistics(Level.from(level), number, 1))
+                .isInstanceOf(BusinessException.class)
+                .hasMessageContaining(INVALID_NUMBER_RANGE.getMessage());
     }
 
 }
